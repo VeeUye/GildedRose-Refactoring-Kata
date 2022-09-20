0c18dc1e3a37cfe11c14a37914e8213978bc7463
Vee Uye <vee@accommodationforstudents.com>
2022-09-20 21:06:03 +0100

    refactor: flatten if statements

--- js-jest/test/gilded_rose.test.js
+++ js-jest/test/gilded_rose.test.js
@@ -1,5 +1,6 @@
 const {Shop, Item} = require("../src/gilded_rose");
 
+
 describe("Gilded Rose", function() {
   it("should foo", function() {
     const gildedRose = new Shop([new Item("foo", 0, 0)]);
