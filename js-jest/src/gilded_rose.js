f10e0b7aef9c5b633b7a6051843453597778b649
Vee Uye <vee@accommodationforstudents.com>
2022-09-20 22:40:52 +0100

    refactor: extract and implement itemCanDecreaseInQualityMethod

--- js-jest/src/gilded_rose.js
+++ js-jest/src/gilded_rose.js
@@ -13,7 +13,7 @@ class Shop {
   updateQuality() {
     for (let i = 0; i < this.items.length; i++) {
       if (this.items[i].name !== 'Aged Brie' && this.items[i].name !== 'Backstage passes to a TAFKAL80ETC concert' && this.items[i].quality > 0 && this.items[i].name !== 'Sulfuras, Hand of Ragnaros') {
-            this.items[i].quality = this.items[i].quality - 1;
+        this.itemCanDecreaseInQuality(this.items[i])
       } else {
         if (this.items[i].quality < 50) {
           this.itemCanIncreaseInQuality(this.items[i])
@@ -37,7 +37,7 @@ class Shop {
         if (this.items[i].name !== 'Aged Brie' && this.items[i].name !== 'Sulfuras, Hand of Ragnaros') {
 
           if (this.items[i].name !== 'Backstage passes to a TAFKAL80ETC concert' && this.items[i].quality > 0) {
-            this.items[i].quality = this.items[i].quality - 1;
+            this.itemCanDecreaseInQuality(this.items[i])
 
           } else {
             this.items[i].quality = this.items[i].quality - this.items[i].quality;
@@ -52,6 +52,10 @@ class Shop {
     }
 
     return this.items;
+  }
+
+  itemCanDecreaseInQuality(item) {
+    item.quality = item.quality - 1;
   }
 
   itemCanIncreaseInQuality(item) {
