965929a3be093fc7e0772339e313f7669ae7f984
Vee Uye <vee.uye@gmail.com>
2022-10-03 22:11:55 +0100

    refactor: bring similar statements together

--- js-jest/src/gilded_rose.js
+++ js-jest/src/gilded_rose.js
@@ -23,17 +23,19 @@ class Shop {
 
       }
       else {
-        if (this.itemCanIncreaseInQuality(this.items[i])) {
-          this.items[i].quality = this.items[i].quality + 1;
-        }
 
-        if (this.items[i].name === 'Backstage passes to a TAFKAL80ETC concert' && this.items[i].sellIn < 11 && this.itemCanIncreaseInQuality(this.items[i])) {
+        if (this.items[i].name === 'Backstage passes to a TAFKAL80ETC concert' && (this.itemCanIncreaseInQuality(this.items[i]))) {
           this.items[i].quality = this.items[i].quality + 1;
-        }
+          if (this.items[i].sellIn < 11 && this.itemCanIncreaseInQuality(this.items[i]))
 
-        if (this.items[i].name === 'Backstage passes to a TAFKAL80ETC concert' && this.items[i].sellIn < 6 && this.itemCanIncreaseInQuality(this.items[i])) {
-          this.items[i].quality = this.items[i].quality + 1;
+            this.items[i].quality = this.items[i].quality + 1;
         }
+        if (this.items[i].sellIn < 6 && this.itemCanIncreaseInQuality(this.items[i])) {
+
+          this.items[i].quality = this.items[i].quality + 1;
+      }
+
+
       }
 
       if ('Sulfuras, Hand of Ragnaros' !== this.items[i].name) {
